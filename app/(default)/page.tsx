60b84543e34f4b43fdc262b591f9fd3c8bbeb3d0
istafamarshall <istafa.a.marshall@gmail.com>
2024-08-19 13:02:07 -0400

    creat business categories

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,15 +5,19 @@ export const metadata = {
 
 import Hero from "@/components/hero-home";
 import HomeHeader from '@/app/components/home-header'
-import BusinessCategories from "@/components/business-categories";
+// import BusinessCategories from "@/components/business-categories";
 import LargeTestimonial from "@/components/large-testimonial";
 import FeaturesPlanet from "@/components/features-planet";
 import Features from "@/components/features-home";
-import TestimonialsCarousel from "@/components/testimonials-carousel";
+// import TestimonialsCarousel from "@/components/testimonials-carousel";
 import Cta from "@/components/cta";
 import ImageSlideShow from "@/app/components/image-slide-show";
 
 import HomePageContent from "@/app/components/home-page-content"
+
+import BusinessCategories from "@/app/components/business-categories";
+
+import TestimonialsCarousel from "@/app/components/testimonials-carousel";
 
 export default function Home() {
   return (
@@ -28,6 +32,11 @@ export default function Home() {
       {/* <HomeHeader /> */}
       <ImageSlideShow />
       <HomePageContent />
+      <BusinessCategories />
+    
+      <Features />
+      <TestimonialsCarousel />
+      
     </>
   );
 }
